e64eabb76e72e2e00bb0dea0b7fe68dbc764ae9c
thuyhang54 <thuyhang8563@gmail.com>
2024-02-29 01:37:33 +0700

    fix udate different address -> doing

--- Controller/thanhtoan.php
+++ Controller/thanhtoan.php
@@ -1,31 +1,33 @@
 <?php 
-
 if(isset($_SESSION['makh'])){
+    $makh = $_SESSION['makh'];
    
-    $makh =$_SESSION['makh'];
+
+    // Kiểm tra nếu có dữ liệu được gửi từ form
+    if(isset($_POST['new_address'])){
+        $kh = new user();
+        $diaChiMoi = $_POST['new_address'];
+        // Cập nhật địa chỉ mới vào tbl_khachhang
+        $kh->updateDiaChiMoi($makh, $diaChiMoi);
+    }
+
     $hd = new hoadon();
     $sohd = $hd->insertHoaDon($makh); 
     // lưu số hóa đơn vào session
-    $_SESSION['masohd']=$sohd;
-    $total=0;
+    $_SESSION['masohd'] = $sohd;
+    $total = 0;
+
     // có được mshd thì insert vào chitiethoadon 
     // chi tiết hóa đơn ? lấy thông tin từ giỏ hàng
     foreach ($_SESSION['cart'] as $key => $item){
-        $hd->insertCTHoaDon($sohd,$item['mahh'],$item['soluong'],$item['mausac'],$item['size'],$item['thanhtien']);
+        $hd->insertCTHoaDon($sohd, $item['mahh'], $item['soluong'], $item['mausac'], $item['size'], $item['thanhtien']);
         $total += $item['thanhtien'];
         // cap nhat so luong ton
-        $hd->updatesoluongton($item['mahh'], $item['mausac'],$item['size'], $item['soluong']);
-      
+        $hd->updatesoluongton($item['mahh'], $item['mausac'], $item['size'], $item['soluong']);
     }
-    $hd->updateHoaDonTongTien($sohd,$makh,$total);
+
+    $hd->updateHoaDonTongTien($sohd, $makh, $total);
     include "./View/confirmation.php";
     unset($_SESSION['cart']);
 }
-// 
-
-
-// include_once "./View/checkout.php";
-// include_once "./View/orderSuccess.php";
-
-
 ?>
--- Model/user.php
+++ Model/user.php
@@ -1,71 +1,103 @@
 <?php
- class user{
+class user
+{
     // Kiểm tra KH = user or email bất kì đã tồn tại chưa?(database)
-    function checkKhachHangUser($user){
+    function checkKhachHangUser($user)
+    {
         $db = new connect();
-        $select ="SELECT a.username FROM tbl_khachhang a WHERE a.username='$user'";
+        $select = "SELECT a.username FROM tbl_khachhang a WHERE a.username='$user'";
         $result = $db->getList($select);
         return $result;
     }
-    function checkKhachHangEmail($email){
+    function checkKhachHangEmail($email)
+    {
         $db = new connect();
-        $select ="SELECT a.email FROM tbl_khachhang a WHERE  a.email='$email'";
+        $select = "SELECT a.email FROM tbl_khachhang a WHERE  a.email='$email'";
         $result = $db->getList($select);
         return $result;
     }
     // Insert vào db nếu KH chưa tồn tại trong db
-    function insertKhachHang($tenkh,$username,$matkhau,$email,$diachi,$sodienthoai) {
+    function insertKhachHang($tenkh, $username, $matkhau, $email, $diachi, $sodienthoai)
+    {
         $db = new connect();
         $query = "INSERT INTO tbl_khachhang (makh,tenkh,username,matkhau,email,diachi,sodienthoai)
         VALUES (NULL,'$tenkh','$username','$matkhau','$email','$diachi','$sodienthoai')";
-        $result =$db->exec($query);
+        $result = $db->exec($query);
         return $result;
     }
     // Kiểm tra người dùng có tồn tại không
-function checkUser($username, $pass) {
-    $db = new connect();
-    $select = "SELECT  a.makh, a.tenkh, a.username FROM tbl_khachhang a WHERE a.username = '$username' AND a.matkhau = '$pass'";
-    $result =$db->getInstance($select);
-    return $result; 
-}
-// Kiểm tra email người dùng có tồn tại không
-function checkEmail($email)
-        {
-            $db=new connect();
-            $select="select * from tbl_khachhang a
+    function checkUser($username, $pass)
+    {
+        $db = new connect();
+        $select = "SELECT  a.makh, a.tenkh, a.username FROM tbl_khachhang a WHERE a.username = '$username' AND a.matkhau = '$pass'";
+        $result = $db->getInstance($select);
+        return $result;
+    }
+    // Kiểm tra email người dùng có tồn tại không
+    function checkEmail($email)
+    {
+        $db = new connect();
+        $select = "select * from tbl_khachhang a
              WHERE a.email='$email'";
-            $result=$db->getList($select);
-            return $result;
-        }
-        // update khi biết được email
-        function updatePassEmail($email,$pass) {
-            $db=new connect();
-            $query = "UPDATE tbl_khachhang SET matkhau='$pass' WHERE email = '$email'";
-            $db->exec($query);
-        }
+        $result = $db->getList($select);
+        return $result;
+    }
+    // update khi biết được email
+    function updatePassEmail($email, $pass)
+    {
+        $db = new connect();
+        $query = "UPDATE tbl_khachhang SET matkhau='$pass' WHERE email = '$email'";
+        $db->exec($query);
+    }
 
-//phương thức hiển thị thông tin khách hàng dựa vào idkh
-function getThongTinUser($idkh)
-{
+    //phương thức hiển thị thông tin khách hàng dựa vào idkh
+    function getThongTinUser($idkh)
+    {
+        $db = new connect();
+        $select = "SELECT tenkh,diachi, email ,sodienthoai FROM tbl_khachhang WHERE makh=$idkh";
+        $result = $db->getInstance($select);
+        return $result;
+    }
+  //Phương thức cập nhật địa chỉ mới vào tbl_khachhang
+function updateDiaChiMoi($idkh, $diachimoi){
     $db = new connect();
-    $select = "SELECT tenkh,diachi, email ,sodienthoai FROM tbl_khachhang WHERE makh=$idkh";
-    $result = $db->getInstance($select);
-    return $result;
+
+    // Lấy địa chỉ ban đầu từ cơ sở dữ liệu
+    $query = "SELECT diachi FROM tbl_khachhang WHERE makh = $idkh";
+    $result = $db->getInstance($query);
+
+    if ($result && $result['diachi'] != $diachimoi){
+        // Cập nhật địa chỉ mới chỉ khi khác với địa chỉ ban đầu
+        $query = "UPDATE tbl_khachhang SET dc_coquan = '$diachimoi' WHERE makh = $idkh";
+        echo $query;
+        $db->exec($query);  
+
+    }
+
+    return true;
 }
 
-//  // Phương thức lấy thông tin hàng hóa theo idKH
-//  function getThongTinCTHH($idkh)
-//  {
-//      $db = new connect();
-//      $select = "SELECT a.tenhh,  c.idmau ,  c.idsize, b.hinh, b.dongia, b.giamgia, c.soluongmua, c.thanhtien 
-//      FROM tbl_hanghoa a JOIN tbl_cthanghoa b ON a.mahh = b.idhanghoa JOIN
-//       tbl_cthoadon c ON a.mahh = c.mahh 
-//       WHERE c.masohd = $masohd
-//        GROUP BY a.mahh;";
-//      $result = $db->getList($select);
-//      return $result;
-//  }
+    // Phương thức lấy địa chỉ mới
+    // function getDiaChiMoi($idkh){
+    //     $db = new connect();
+    //     $select = "SELECT dc_coquan FROM tbl_khachhang WHERE makh =$idkh";
+    //     $result = $db->getInstance( $select );
+    //     return $result['dc_coquan'];
+    // }
+
+    //  // Phương thức lấy thông tin hàng hóa theo idKH
+    //  function getThongTinCTHH($idkh)
+    //  {
+    //      $db = new connect();
+    //      $select = "SELECT a.tenhh,  c.idmau ,  c.idsize, b.hinh, b.dongia, b.giamgia, c.soluongmua, c.thanhtien 
+    //      FROM tbl_hanghoa a JOIN tbl_cthanghoa b ON a.mahh = b.idhanghoa JOIN
+    //       tbl_cthoadon c ON a.mahh = c.mahh 
+    //       WHERE c.masohd = $masohd
+    //        GROUP BY a.mahh;";
+    //      $result = $db->getList($select);
+    //      return $result;
+    //  }
+    
 
 
- } 
-?>
+}
